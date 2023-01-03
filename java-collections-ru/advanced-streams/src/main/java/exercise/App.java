e76201dd9cc4a68559d3ff39d1597008a542ead0
@hexlet/cli <support@hexlet.io>
2023-01-04 00:55:48 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -1,26 +1,27 @@
 package exercise;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 // BEGIN
 public class App {
     public static String getForwardedVariables(String data) {
-        StringBuilder result = new StringBuilder();
-        getEnvironments(data).forEach(
-                env -> getVariables(env)
-                        .filter(variable -> variable.startsWith("X_FORWARDED_"))
-                        .forEach(forwarded -> result.append(getName(forwarded)).append(","))
-        );
-        return result.substring(0, result.length() - 1);
+        return getVariables(getEnvironments(data))
+                .filter(variable -> variable.startsWith("X_FORWARDED_"))
+                .map(App::getName)
+                .collect(Collectors.joining(","));
     }
 
     private static Stream<String> getEnvironments(String data) {
         return data.lines().filter(line -> line.startsWith("environment="));
     }
 
-    private static Stream<String> getVariables(String env) {
-        return Arrays.stream(env.substring(env.indexOf("\"") + 1, env.length() - 1).split(","));
+    private static Stream<String> getVariables(Stream<String> environments) {
+        return environments
+                .map(env -> env.substring(env.indexOf("\"") + 1, env.length() - 1))
+                .map(l -> l.split(","))
+                .flatMap(Arrays::stream);
     }
 
     private static String getName(String forwardedVariable) {
